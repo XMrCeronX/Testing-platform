7717425270d60aa03c4acad15f8c1f52fcad0803
ARTEM <artemkakrg@gmail.com>
2024-11-01 18:32:53 +0300

    добавил ProgressBar вместе с привязкой свойств

--- Test platform/ViewModels/LoginViewModel.cs
+++ Test platform/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 using Test_platform.Infrastructure.Commands;
 using Test_platform.ViewModels.Base;
 
@@ -31,15 +33,72 @@ namespace Test_platform.ViewModels
         private bool CanLogInCommand(object parameters) => true;
         private void OnLogInCommand(object parameters)
         {
+            ChangeProgressBarVisibility();
             string password = (parameters as PasswordBox).Password; // немного нарушил MVVM (LoginViewModel знает о PasswordBox т.е. знает о View)
-            MessageBox.Show($"{_login}\n{password}");
+            //MessageBox.Show($"{Login}\n{password}");
         }
         #endregion
+
+        #region ProgressBarValue
+        private int _progressBarValue = 0;
+        public int ProgressBarValue
+        {
+            get => _progressBarValue;
+            set => Set(ref _progressBarValue, value);
+        }
+        #endregion
+
+        #region ProgressBarVisibility
+        private Visibility _progressBarVisibility = Visibility.Visible; // Visibility.Collapsed
+        public Visibility ProgressBarVisibility
+        {
+            get => _progressBarVisibility;
+            set => Set(ref _progressBarVisibility, value);
+        }
+        #endregion
+
+        private void ChangeProgressBarVisibility()
+        {
+            switch (ProgressBarVisibility)
+            {
+                case Visibility.Visible:
+                    ProgressBarVisibility = Visibility.Collapsed; // Не отображайте элемент и не резервируйте для него место в макете.
+                    break;
+                case Visibility.Collapsed:
+                    ProgressBarVisibility = Visibility.Visible; // Отображай элемент.
+                    break;
+                default:
+                    ProgressBarVisibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        Random rand = new Random();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(5) // Интервал срабатывания таймера
+        };
+
+        private void CreateTaskProgressBarValueUpdate()
+        {
+            dispatcherTimer.Tick += (object sender, EventArgs e) =>
+            {
+                int newMumber = rand.Next(1, 100);
+                ProgressBarValue = newMumber;
+            };
+            dispatcherTimer.Start();
+        }
 
         public LoginViewModel()
         {
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommand, CanCloseApplicationCommand);
             LogInCommand = new LambdaCommand(OnLogInCommand, CanLogInCommand);
+            CreateTaskProgressBarValueUpdate();
+        }
+
+        ~LoginViewModel()
+        {
+            dispatcherTimer.Stop();
         }
     }
 }
