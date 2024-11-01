c708700ea324703d19c372cfe6344baebe77c74b
ARTEM <artemkakrg@gmail.com>
2024-11-01 15:38:52 +0300

    добавил Login View

---
+++ Test platform/Views/Windows/Login.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Test_platform.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
