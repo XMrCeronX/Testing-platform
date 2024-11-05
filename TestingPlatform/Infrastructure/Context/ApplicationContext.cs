b1ae9ca757a49cb3ad361ba447051fd46a0785e1
ARTEM <artemkakrg@gmail.com>
2024-11-06 00:11:39 +0300

    добавил получение ConnectionStrings из appsettings.json

--- TestingPlatform/Infrastructure/Context/ApplicationContext.cs
+++ TestingPlatform/Infrastructure/Context/ApplicationContext.cs
@@ -1,10 +1,15 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using System.Windows.Controls;
 using TestingPlatform.ViewModels.Base;
+using System.Linq;
 
 namespace TestingPlatform.Infrastructure.Context
 {
     internal class ApplicationContext
     {
+
         public Page CurrentPage { get; set; }
         public ViewModel CurrentViewModel { get; set; }
 
@@ -13,6 +18,18 @@ namespace TestingPlatform.Infrastructure.Context
         {
             CurrentPage = currentPage;
             CurrentViewModel = currentViewModel;
+        }
+
+        public static string GetConnectionString()
+        {
+            string[] paths = AppDomain.CurrentDomain.BaseDirectory.Split("\\").SkipLast(4).ToArray();
+            string pathToRoot = Path.Combine(paths);
+            string configFilePath = Path.Combine(pathToRoot, "Config\\appsettings.json");
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(configFilePath, optional: false, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            string connectionString = configuration["ConnectionStrings:DefaultConnection"];
+            return connectionString;
         }
     }
 }
