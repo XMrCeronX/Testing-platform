a1a58bdedaccb63707d3508c98f56a62c25cd090
ARTEM <artemkakrg@gmail.com>
2024-11-06 00:09:59 +0300

    добавил класс Cryptography для получения хеша по строке

---
+++ TestingPlatform/Infrastructure/Cryptography/Cryptography.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestingPlatform.Infrastructure.Cryptography
+{
+    internal class Cryptography
+    {
+
+        public static string CreateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+            }
+        }
+
+    }
+}
