2fa637c48c9e42711e33522c654c2bdf50449f8d
ARTEM <artemkakrg@gmail.com>
2024-11-06 23:44:19 +0300

    добавил возможность не только логирования, но и показа сообщения пользователю

--- TestingPlatform/Infrastructure/Logging/Logger.cs
+++ TestingPlatform/Infrastructure/Logging/Logger.cs
@@ -12,30 +12,34 @@ namespace TestingPlatform.Infrastructure.Logging
                 new FileHandler(AppDomain.CurrentDomain.BaseDirectory)
         );
 
-        public static void Debug(string message, bool usingMessageBox = false)
+        public static void Debug(string message, bool usingMessageBox = false, string caption = "Сообщение отладки")
         {
             handlerManager.Write(message, LogLevel.Debug);
-            if (usingMessageBox) MessageBox.Show(message);
+            if (usingMessageBox) MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.None);
         }
 
-        public static void Info(string message)
+        public static void Info(string message, bool usingMessageBox = false, string caption = "Сообщение")
         {
             handlerManager.Write(message, LogLevel.Info);
+            if (usingMessageBox) MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
-        public static void Warning(string message)
+        public static void Warning(string message, bool usingMessageBox = false, string caption = "Предупреждение")
         {
             handlerManager.Write(message, LogLevel.Warning);
+            if (usingMessageBox) MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
-        public static void Error(string message)
+        public static void Error(string message, bool usingMessageBox = false, string caption = "Ошибка")
         {
             handlerManager.Write(message, LogLevel.Error);
+            if (usingMessageBox) MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
-        public static void Critical(string message)
+        public static void Critical(string message, bool usingMessageBox = false, string caption = "Критическая ошибка")
         {
             handlerManager.Write(message, LogLevel.Critical);
+            if (usingMessageBox) MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
