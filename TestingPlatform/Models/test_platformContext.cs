0fc0be005cebe2850439bd6ebebecb22bb33f5d5
ARTEM <artemkakrg@gmail.com>
2024-11-21 13:35:50 +0300

    добавил получение строки подключения к БД через статичный метод

--- TestingPlatform/Models/test_platformContext.cs
+++ TestingPlatform/Models/test_platformContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using TestingPlatform.Infrastructure.Context;
+using TestingPlatform.Database;
 
 namespace TestingPlatform.Models
 {
@@ -26,7 +26,7 @@ namespace TestingPlatform.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql(ApplicationContext.GetConnectionString(), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
+                optionsBuilder.UseMySql(DataBaseConnection.GetConnectionString(), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
             }
         }
 
