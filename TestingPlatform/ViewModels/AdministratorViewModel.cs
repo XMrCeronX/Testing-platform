99d38253d762f90892a75d69b37327910ad33c57
ARTEM <artemkakrg@gmail.com>
2024-11-23 12:55:32 +0300

    добавил страницу и модель для администратора

---
+++ TestingPlatform/ViewModels/AdministratorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+using TestingPlatform.Infrastructure.Commands;
+using TestingPlatform.Infrastructure.Navigation;
+using TestingPlatform.ViewModels.Base;
+using TestingPlatform.Views.Pages;
+
+namespace TestingPlatform.ViewModels
+{
+    internal class AdministratorViewModel : ViewModel
+    {
+        #region SignOutCommand
+        public ICommand SignOutCommand { get; }
+        private bool CanSignOutCommand(object parameters) => true;
+        private void OnSignOutCommand(object parameters)
+        {
+            NavigationService.Instance.Navigate(new LoginPage(new LoginViewModel()), null);
+        }
+        #endregion
+
+        public AdministratorViewModel()
+        {
+            SignOutCommand = new LambdaCommand(OnSignOutCommand, CanSignOutCommand);
+        }
+    }
+}
